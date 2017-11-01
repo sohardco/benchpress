02ebbdf8798e8f2d07a177c40c1d077e76e760c6
Yaroslav Maistrenkov <sohardco@MacBook-Pro-Yaroslav.local>
2017-11-02 00:08:27 +0200

    added day to ratio logic

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
 const functions = require('./functions');
 
-var workoutDays = 12;
+var wholeWorkout = 12;
 var initialBarbellweight =  document.getElementById('inputWeight');
-var defaultWeightRatio = [0.3, 0.45, 0.6, 0.7, 0.8, 0.84, 0.88, 0.92, 0.96];
-var lastWorkoutDayRatio =[0.36, 0.54, 0.68, 0.76, 0.84, 0.92, 1.0, 1.0, 1.0, 1.0];
+const defaultWeightRatios = [0.3, 0.45, 0.6, 0.7, 0.8, 0.84, 0.88, 0.92, 0.96];
+const lastWorkoutDayRatios =[0.36, 0.54, 0.68, 0.76, 0.84, 0.92, 1.0, 1.0, 1.0, 1.0];
 
 //Доп. коеффициенты для формирования полного колличества весов для тренировки
-var additionalRatios = [0.8, 0.84, 0.86, 0.88, 0.9, 0.92];
+const additionalRatios = [0.8, 0.82, 0.84, 0.86, 0.88, 0.9, 0.92, 1.0];
 
 
 
-for (workoutDay = 0; i < wholeWorkout; i++){
-    switch (workoutDay) {
-        case "1":
-            var secondDayWorkoutRatio = defaultWeightRatio.concat(additionalRatios.slice(0,2).reverse());
-            break;
-        case "2":
-            var thirdDayWorkoutRatio = defaultWeightRatio.slice(0, -2).concat(additionalRatios.slice(0,2).reverse());
-            break;
-        case "3":
-            var fourthDayWorkoutRatio = defaultWeightRatio.slice(0, -1).concat(additionalRatios.slice(0,2).reverse());
-            break;
-        case "4":
-            var fourthDayWorkoutRatio = defaultWeightRatio.slice(0, -1).concat(additionalRatios.slice(0,2).reverse());
-            break;
-        default:
-            var firstDayWorkoutRatio = defaultWeightRatio.slice(0, -2).concat(additionalRatios.slice(0,2).reverse());
-    }
+for (workoutDay = 0; workoutDay < wholeWorkout; workoutDay++){
+
 
 }
 
