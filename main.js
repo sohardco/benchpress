6853b4b7ea0e45d882d24e1fe4a6a89711500b95
Yaroslav Maistrenkov <sohardco@MacBook-Pro-Yaroslav.local>
2017-11-06 00:07:46 +0200

    created map

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const functions = require('./functions');
 
-var wholeWorkout = 12;
 var initialBarbellweight =  document.getElementById('inputWeight');
-const defaultWeightRatios = [0.3, 0.45, 0.6, 0.7, 0.8, 0.84, 0.88, 0.92, 0.96];
-const lastWorkoutDayRatios =[0.36, 0.54, 0.68, 0.76, 0.84, 0.92, 1.0, 1.0, 1.0, 1.0];
-
-//Доп. коеффициенты для формирования полного колличества весов для тренировки
-const additionalRatios = [0.8, 0.82, 0.84, 0.86, 0.88, 0.9, 0.92, 1.0];
-
-
-
-for (workoutDay = 0; workoutDay < wholeWorkout; workoutDay++){
-
-
+var workoutRatios = {
+    warmUp:[0.3, 0.45, 0.6, 0.7, 0.8, 0.84, 0.88],
+    firstDay:[0.84, 0.8],
+    secondDay:[0.92, 0.96, 0.84, 0.8],
+    thirdDay:[0.92, 0.84, 0.8],
+    fourthDay:[0.92, 0.96, 1, 0.86, 0.8],
+    fifthDay:[0.92, 0.86, 0.8],
+    sixthDay:[0.92, 0.96, 0.86, 0.8],
+    seventhDay:[0.92, 0.96, 1, 0.88, 0.82],
+    eightsDay:[0.92, 0.96, 0.88, 0.82],
+    ninthDay:[0.92, 0.96, 1, 0.90, 0.82],
+    tenthDay:[0.92, 0.96, 1, 0.92, 0.84],
+    eleventhDay:[0.3, 0.45, 0.6, 0.7],
+    lastDay:[0.36, 0.54, 0.68, 0.76, 0.84, 0.92, 1.0]
 }
+
 
 functions.calculateWeights(initialBarbellweight,weightRatio);
