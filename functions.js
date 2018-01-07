0f9a8d1473628432168cc871452d0f0359d18510
Yaroslav Maistrenkov <sohardco@MacBook-Pro-Yaroslav.local>
2018-01-08 00:12:58 +0200

    redo calculateweights function

--- functions.js
+++ functions.js
@@ -1,28 +1,31 @@
-var trainingWeight = [];
+var calculatedBarbellWeights = [];
 var concatDays = [];
+//concatDays array of arrays
 
-function calculateWeights(weight,concatDays){
+export function calculateWeights(weight, concatDays) {
 
-    for (var count=0; count < Object.keys(concatDays).length; count++) {
-        var ratio = Object.values(concatDays)[count];
-        var fractionalWeight = (weight * ratio[count])/2.5;
-        trainingWeight[count] = Math.round(fractionalWeight)*2.5;
-    }
-    return trainingWeight;
-}
-
-function concatTrainingRatios(warmUp, scaleObj){
-    for (var i = 0; i < (Object.values(scaleObj).length); i++) {
-        if (Object.keys(scaleObj)[i] === "eleventhDay"){
-            concatDays[i] = Object.values(scaleObj)[i];
-        }else if (Object.keys(scaleObj)[i] === "lastDay") {
-            concatDays[i] = Object.values(scaleObj)[i];
-        }else{
-            concatDays[i] = warmUp.concat(Object.values(scaleObj)[i]);
+    for (var count = 0; count < concatDays.length; count++) {
+        var trainingWeight = [];
+        var ratio = concatDays[count];
+        for (var i = 0; i < ratio.length; i++) {
+            for (var i = 0; i < ratio.length; i++) {
+                trainingWeight[i] = (Math.round((weight * ratio[i]) / 2.5) * 2.5);
+            }
+            calculatedBarbellWeights[count] = trainingWeight;
         }
-
+        return calculatedBarbellWeights;
     }
-    return concatDays;
-}
 
-export {calculateWeights, concatTrainingRatios};
+export    function concatTrainingRatios(warmUp, scaleObj) {
+        for (var i = 0; i < (Object.values(scaleObj).length); i++) {
+            if (Object.keys(scaleObj)[i] === "eleventhDay") {
+                concatDays[i] = Object.values(scaleObj)[i];
+            } else if (Object.keys(scaleObj)[i] === "lastDay") {
+                concatDays[i] = Object.values(scaleObj)[i];
+            } else {
+                concatDays[i] = warmUp.concat(Object.values(scaleObj)[i]);
+            }
+
+        }
+        return concatDays;
+    }
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//import { calculateWeights, concatTrainingRatios } from './functions';
-
+import{
+    calculateWeights,
+    concatTrainingRatios
+}from './functions';
 
 var warmUp = [0.3, 0.45, 0.6, 0.7, 0.8, 0.84, 0.88];
-
 var workoutRatios = {
     firstDay:[0.84, 0.8],
     secondDay:[0.92, 0.96, 0.84, 0.8],
@@ -17,31 +18,3 @@ var workoutRatios = {
     eleventhDay:[0.3, 0.45, 0.6, 0.7],
     lastDay:[0.36, 0.54, 0.68, 0.76, 0.84, 0.92, 1.0]
 };
-
-var trainingWeight = [];
-var concatDays = {};
-
-function calculateWeights(weight,ratio){
-    for (var count=0; count < Object.values(ratio).length; count++) {
-        var transformArray = Object.values(ratio)[count];
-        var fractionalWeight = (weight * transformArray[count])/2.5;
-        trainingWeight[count] = Math.round(fractionalWeight)*2.5;
-    }
-    return trainingWeight;
-}
-
-function concatTrainingRatios(warmUp, scaleObj){
-    for (var i = 0; i < (Object.values(scaleObj).length); i++) {
-        if (Object.keys(scaleObj)[i] === "eleventhDay"){
-            concatDays[i] = Object.values(scaleObj)[i];
-        }else if (Object.keys(scaleObj)[i] === "lastDay") {
-            concatDays[i] = Object.values(scaleObj)[i];
-        }else{
-            concatDays[i] = warmUp.concat(Object.values(scaleObj)[i]);
-        }
-
-    }
-    return concatDays;
-}
-
-//export {calculateWeights, concatTrainingRatios};
