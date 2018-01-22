7f6f457ee24ad6d1ed598b60114e6b2a5ae1a8ce
Yaroslav Maistrenkov <sohardco@MacBook-Pro-Yaroslav.local>
2018-01-22 09:15:02 +0200

    create table function add

--- functions.js
+++ functions.js
@@ -30,13 +30,14 @@ function concatTrainingRatios(warmUp, scaleObj) {
 
 
 function createTableFromObj(tableObj) {
-    var tdCount = 3;
     var body = document.body;
     var tbl = document.createElement("table");
+    tbl.className = tableObj[0];
     var th = tbl.createTHead();
-    //tbl.style.display = 'none';
+    var tdCount = 3;
+    var trCount = tableObj[1].length;
 
-    for (var i = 0; i < day.length; i++) {
+    for (var i = 0; i < trCount; i++) {
         var tr = tbl.insertRow();
         for (var j = 0; j < tdCount; j++) {
             var td = tr.insertCell();
@@ -46,12 +47,20 @@ function createTableFromObj(tableObj) {
     body.appendChild(tbl);
 }
 
-// function fillTable(calculatedBarbellWeights) {
-//     for (var i = 0; i < calculatedBarbellWeights.length; i++) {
-//         var fillingArray = calculatedBarbellWeights[i];
-//     for (var i = 0; i < fillingArray.length; i++) {
-//         document.getElementsByName("weight")[i].innerHTML = fillingArray[i];
-//         }
+function fillTable(calculatedBarbellWeights) {
+    var iterations = Object.keys(calculatedBarbellWeights).length
+    for (var i = 0; i < iterations ; i++) {
+        var tableData = Object.entries(calculatedBarbellWeights)[i];
+        createTableFromObj(tableData);
+
+
+    }
+};
+
+// for (var i = 0; i < calculatedBarbellWeights.length; i++) {
+//     var fillingArray = calculatedBarbellWeights[i];
+// for (var i = 0; i < fillingArray.length; i++) {
+//     document.getElementsByName("weight")[i].innerHTML = fillingArray[i];
 //     }
-// };
+// }
 //export {calculateWeights, concatTrainingRatios};
