3629986a4e78672195242195c37dc10887299a48
Yaroslav Maistrenkov <sohardco@MacBook-Pro-Yaroslav.local>
2018-02-05 01:52:55 +0200

    next button implementation

--- scripts/main.js
+++ scripts/main.js
@@ -19,26 +19,34 @@ function app() {
 }
 
 function goToNextPage() {
-var nodeTablesRendered = document.querySelectorAll('.js-table');
-var arrTablesRendered = Array.prototype.slice.call(nodeTablesRendered);
+    var nodeTablesRendered = document.querySelectorAll('.js-table');
+    var arrTablesRendered = Array.prototype.slice.call(nodeTablesRendered);
 
-function returnDisplayElement(element){
-	var regCheck = /(hidden)/gi;
-	if (!regCheck.test(element.classList.value)){
-	return element.classList.value
-	}
-};
+    function returnDisplayElement(element){
+	    var regCheck = /(hidden)/gi;
+	    if (!regCheck.test(element.classList.value)){
+	       return element.classList.value
+	    }
+    };
 
-var getDisplayIndex = arrTablesRendered.findIndex(returnDisplayElement);
+    var displayIndex = arrTablesRendered.findIndex(returnDisplayElement);
 
-switch (getDisplayIndex) {
-    case 0:
-        prevBtn.classList.toggle('hidden', true);
-        break;
-    case arrTablesRendered.length:
-        nextBtn.classList.toggle('hidden', true);
-        break;
-}
+    switch (displayIndex) {
+        case 0:
+            prevBtn.classList.toggle('hidden', true);
+            break;
+        case 10:
+            nextBtn.classList.toggle('hidden', true);
+            break;
+        default:
+            prevBtn.classList.toggle('hidden', false);
+            nextBtn.classList.toggle('hidden', false);
+    }
+    if(displayIndex != 11){
+        arrTablesRendered[displayIndex].classList.toggle('hidden', true);
+        arrTablesRendered[displayIndex += 1].classList.toggle('hidden', false);
+    }
+
 }
 // if (getDisplayIndex === 0) {
 //     prevBtn.classList.toggle('hidden', true);
